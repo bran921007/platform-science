bd633a458efa492ddcc913ea6017a9cf0aaf5086
Dan Unite <danilo.unite@gmail.com>
2021-08-13 12:45:29 -0700

    Added inquirer dependency to provide prompting for input file paths. Modified TypeScript compiler options to allow for using async/await syntax and remove unneeded option value

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,47 @@
-const [drivers, destinations] = readDriversAndDestinations();
 
-const driverAssignments = mapDriversToDestinations(drivers, destinations);
 
-function readDriversAndDestinations() {
+async function main() {
+    const [drivers, destinations] = await readDriversAndDestinations();
+    const driverAssignments = mapDriversToDestinations(drivers, destinations);
+}
+
+main();
+
+async function readDriversAndDestinations () {
     // TODO
-    console.log(process.argv);
+    const inquirer = require('inquirer');
+    const answers = await inquirer.prompt(
+        [
+            {
+                name: 'driversListPath',
+                message: 'Enter the path to the drivers list file'
+            },
+            {
+                name: 'destinationsListPath',
+                message: 'Enter the path to the destinations list file'
+            }
+        ]
+    );
+    const {driversListPath, destinationsListPath} = answers;
+    console.log('', driversListPath,'\n',destinationsListPath);
+
     const drivers: string[] = [];
     const destinations: string[] = [];
     return [drivers, destinations];
 }
 
-function mapDriversToDestinations(names: Iterable<String>, addresses: Iterable<String>) {
+function mapDriversToDestinations (
+    names: Iterable<String>,
+    addresses: Iterable<String>
+) : {[driver: string]: [destination: string]} {
     // TODO
+    const scores: {[name: string]: number} = {};
     // throw new Error(`${names}:${addresses}`);
+    return {};
+}
+
+function initializeRewardMatrix () {
+
 }
 
 function calculateSuitabilityScore(name: string, address: string) {
