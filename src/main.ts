081943bd7392de0541977d9ef22afca854dfe9ed
Dan Unite <danilo.unite@gmail.com>
2021-08-15 15:57:27 -0700

    Re-organized and refactored code. Added declaration file for munkres-js package to allow it to be imported as ES module. Implemented logic to use munkres-js module to calculate optimization of suitability scores for driver assignments

---
+++ src/main.ts
@@ -0,0 +1,177 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+import readline from 'readline';
+import munkres from 'munkres-js';
+import { IAssignment } from './models';
+
+const {drivers, destinations} = await readDriversAndDestinations();
+const {totalScore, assignments} = assignDriversToDestinations(drivers, destinations);
+
+async function readDriversAndDestinations (
+) : Promise<{drivers: string[], destinations: string[]}> {
+
+    const input = await inquirer.prompt(
+        [
+            {
+                name: 'destinationsListPath',
+                message: 'Enter the relative path to the destinations list file'
+            },
+            {
+                name: 'driversListPath',
+                message: 'Enter the relative path to the drivers list file'
+            }
+        ]
+    );
+
+    const {destinationsListPath, driversListPath} = input;
+
+    const destinations = await getContents(destinationsListPath);
+    const drivers = await getContents(driversListPath);
+
+    console.log(`drivers: ${drivers}`);
+    console.log(`destinations: ${destinations}`);
+    return {drivers, destinations};
+}
+
+async function getContents (filepath: string
+) : Promise<string[]> {
+    const listIterable = readline.createInterface({
+        input: fs.createReadStream(filepath),
+        crlfDelay: Infinity
+    });
+
+    const contents: string[] = [];
+    for await (const item of listIterable) {
+        contents.push(item);
+    }
+
+    return contents;
+}
+
+function assignDriversToDestinations (
+    names: Array<string>,
+    addresses: Array<string>
+) : {totalScore: number, assignments: Map<string, string>} {
+
+    // Create reward and cost matrices. The reward matrix is generated from
+    // using the suitability score algorithm provided. The cost matrix is
+    // derived from the reward matrix by subtracting all scores in the reward
+    // matrix from the same arbitrarily large number. 
+    const n = names.length;
+    const namesToAddressesMap = initMatrix(n, {});
+    const {rewards, costs} = createRewardAndCostMatrices(
+        names, addresses, namesToAddressesMap
+    );
+
+    // Use derived cost matrix to obtain the optimized driver/destination
+    // pairings in the form of an array of all matrix positions (i.e. [i,j])
+    // that correspond to optimized pairings. Again, these optimized cost
+    // pairings correspond to the optimized reward pairings in the original
+    // reward matrix, so the indices are the same
+    const assignmentIndices = munkres(costs);
+
+    // Calculate the total suitability score from the assignments and the
+    // original reward matrix
+    const totalScore = calculateTotalSuitabilityScore(
+        rewards, assignmentIndices
+    );
+
+    // TODO
+    const assignments: Map<string, string> = new Map<string, string>();
+    return {totalScore, assignments};
+}
+
+function createRewardAndCostMatrices (
+    names: Array<string>,
+    addresses: Array<string>,
+    namesToAddressesMap: Array<Array<IAssignment>>
+    
+) : {rewards: Array<Array<number>>, costs: Array<Array<number>>} {
+
+    // Initialize the n x n reward matrix with zeros
+    const n = addresses.length;
+    const rewardMatrix: Array<Array<number>> = initMatrix(n, new Number(0));
+
+    // Construct reward matrix, as well as fill mapping of all pairs of driver
+    // names to destination addresses. This will be used to make the assignments
+    names.forEach((name, row) => {
+        addresses.forEach((address, column) => {
+            rewardMatrix[row][column] = calculateSuitabilityScore(name, address);
+            namesToAddressesMap[row][column] = {driver: name, destination: address};
+        });
+    })
+
+    // Convert reward matrix to cost matrix
+    const costMatrix = rewardMatrix.map((row) => {
+        return row.map((score) => {
+            // Use arbitrarily large value to subtract from
+            return 1000 - score;
+        });
+    });
+
+    return {rewards: rewardMatrix, costs: costMatrix};
+}
+
+function calculateTotalSuitabilityScore (
+    scores: Array<Array<number>>,
+    indices: Array<Array<number>>
+) : number {
+    // TODO
+    return indices.reduce((totalScore, current) => {
+        return totalScore + scores[current[0]][current[1]];
+    }, 0);
+}
+
+function initMatrix (size: number, initValue: object) {
+    return new Array(size).fill(0).map((item: number) => {
+        return new Array(size).fill(initValue);
+    });
+}
+
+function calculateSuitabilityScore (name: string, address: string): number {
+    // Calculate the base suitability score based on the address length
+    // and the number of vowels in the name
+    let score = address.length % 2 == 0 ? 
+        getNumberOfVowels(name) * 1.5 // Even address length
+        :
+        getNumberOfConsonants(name) // Odd address length
+
+    // Augment score if address length shares common factors with name length
+    return hasLengthWithCommonFactors(name, address) ?
+        score * 1.5 // Increase by 50% above base SS
+        :
+        score;
+}
+
+function getNumberOfVowels (name: string): number {
+    const m = name.match(/[aeiou]/gi);
+    return m === null ? 0 : m.length;
+}
+
+function getNumberOfConsonants (name: string): number {
+    const m = name.match(/[bcdfghjklmnpqrstvwxyz]/gi);
+    return m === null ? 0 : m.length;
+}
+
+function hasLengthWithCommonFactors(name: string, address: string): boolean {
+    // Normalize the names and addresses by stripping any whitespace
+    const strippedName = stripWhiteSpace(name);
+    const strippedAddress = stripWhiteSpace(address);
+
+    const addressLengthFactors = getFactorsBesides1(strippedAddress.length);
+    const nameLengthFactors = getFactorsBesides1(strippedName.length);
+
+    return addressLengthFactors.some(factor => nameLengthFactors.includes(factor));
+}
+
+function stripWhiteSpace (str: string) {
+    return str.replace(/\s+/g,'');
+}
+function getFactorsBesides1 (length: number) {
+    return Array
+        // Create sequence of numbers, ignoring 1, up to the given number
+        // e.g. 7 -> [2,3,4,5,6,7]
+        .from(Array(length), (_, i) => i + 2)
+        // Filter for numbers in the sequence that the given number is divisible by
+        .filter(i => length % i === 0);
+}
